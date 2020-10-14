b31dbc9c30d5425e4f20f56c906b932d84d253b6
leej1012 <leej1012@aliyun.com>
2020-10-14 14:55:34 +0800

    Update

--- src/main/java/io/ont/demo/AttestationDemo.java
+++ src/main/java/io/ont/demo/AttestationDemo.java
@@ -16,19 +16,18 @@ public class AttestationDemo {
 //        AttestationSdk sdk = new AttestationSdk("C:\\Users\\xxx\\wallet.dat", 0, "123456");
 
         // 2.create sdk instance by mnemonic codes
-//        AttestationSdk sdk = new AttestationSdk("brother cube work silk eagle arrange minute eyebrow episode defense agree title");
-        AttestationSdk sdk = new AttestationSdk("mnemonicCodes");
+//        AttestationSdk sdk = new AttestationSdk("mnemonicCodes");
 
         // 3.create sdk instance by key store
-//        AttestationSdk sdk = new AttestationSdk("{\"scrypt\":{\"dkLen\":64,\"n\":16384,\"p\":8,\"r\":8},\"address\":\"AXVvpDHbVhFBLMos2H5ZcA3yEYYx5Q8kSW\",\"key\":\"ea5vQNxYqRx0yya8y6qkdmX/W38I98qXOAJHV0E/84kUqD1pJsPprz1AFSlmuWEf\",\"label\":\"leeMain\",\"type\":\"I\",\"algorithm\":\"ECDSA\",\"salt\":\"qvBeCxzfL3/mPPT9WTOQ9A==\",\"parameters\":{\"curve\":\"P-256\"}}", "123456");
+        AttestationSdk sdk = new AttestationSdk("", "");
 
         // testNode:http://polaris1.ont.io:20336
         // mainNode:http://dappnode1.ont.io:20336
-        sdk.initialize("http://172.168.3.46:2020/addon/attestation",
+        sdk.initialize("http://106.75.209.209:2020/addon/attestation",
                 "http://polaris2.ont.io:20336",
-                "did:ont:AYKUPMjmfZhqGFb4nNjTFd4oqbtHe2dmyY",
-                "15",
-                "f0d888088b4d968f250737f6ea656abc38c77ce4");
+                "did:ont:AG7o6vm4AZ7YMvfHskui2mk2PP9nEvdy2i",
+                "8",
+                "0e116f28e32762a12ac999a54e416f1025a2d148");
 
         // get Contract address (if necessary)
 //        sdk.getContractAddress();
@@ -76,9 +75,9 @@ public class AttestationDemo {
         }
 
 //         get hashes by txHash
-        List<String> hashes = sdk.getHashesByTxHash("txHash");
-        for (String hash : hashes) {
-            System.err.println(hash);
-        }
+//        List<String> hashes = sdk.getHashesByTxHash("txHash");
+//        for (String hash : hashes) {
+//            System.err.println(hash);
+//        }
     }
 }
